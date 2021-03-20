b203b776b3ae6d89bac8630e1fcf9451d7cf891d
dameo211 <DameO211@gmail.com>
2021-03-20 16:13:48 -0400

    Ready for submission

---
+++ .idea/fileTemplates/Header.java
@@ -0,0 +1,5 @@
+/**
+ * @author : Cole,Dameon
+ * Section: CMIS 242/7383
+ * @since : ${DATE}, ${DAY_NAME_SHORT}
+**/
--- src/Microwave.java
+++ src/Microwave.java
@@ -1,32 +1,35 @@
+//*************************************
+// Author: Cole, Dameon
+// Course: CMIS 242 7383
+// Date :  03/20/2021
+// ************************************
+
 import java.util.Scanner;
 
 public class Microwave {
-//    attributes
-    public int timer;
-    public boolean isWarm;
+    //       set boolean
+    //    attributes
+    public int timer = 0;
+    public boolean isWarm = false;
 
-//    constructor
+    //    constructor
     public Microwave() {
-        this.timer = timer;
 
-//        if food is warm or cold
-        isWarm = false;
     }
 
-    public void cookTimer(){
+    public void cookTimer() {
         Scanner cookTime = new Scanner(System.in);
         timer = cookTime.nextInt();
 
 
-        if(this.timer>=70) {
+        if (timer >= 70) {
             isWarm = true;
             System.out.println("Food is warm");
-        }
-        else System.out.println("Food is still too cold");
+        } else System.out.println("Food is still too cold");
     }
 
-    public static class Driver{
-        public static void main(String[] args){
+    public static class Driver {
+        public static void main(String[] args) {
             Microwave GE = new Microwave();
             GE.cookTimer();
         }
